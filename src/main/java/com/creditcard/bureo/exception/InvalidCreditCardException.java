d46871ab451ef9690123b9994ca1186978c4811e
Ravi <ireddyravikumar88@gmail.com>
2018-04-06 00:37:35 -0500

    Update InvalidCreditCardException.java
    CreditBureoApp

--- src/main/java/com/creditcard/bureo/exception/InvalidCreditCardException.java
+++ src/main/java/com/creditcard/bureo/exception/InvalidCreditCardException.java
@@ -2,9 +2,16 @@ package com.creditcard.bureo.exception;
 
 public class InvalidCreditCardException extends RuntimeException {
 	
+	private String errorMsg;
+	
 	public InvalidCreditCardException(){
 		
 		super();
+	}
+	
+	public InvalidCreditCardException(String errorMessage){
+		super(errorMessage);
+		this.errorMsg=errorMessage;
 	}
 
 }
